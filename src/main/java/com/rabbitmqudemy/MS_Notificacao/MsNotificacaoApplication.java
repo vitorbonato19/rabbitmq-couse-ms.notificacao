8f8db9ffe5057aa5dabe333e1f0c9b41b2f23635
Vitor Bonato <vitorbonatocam@gmail.com>
2024-07-07 21:36:27 -0300

    first commit

---
+++ src/main/java/com/rabbitmqudemy/MS_Notificacao/MsNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmqudemy.MS_Notificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbitmqudemy/MS_Notificacao/MsNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmqudemy.MS_Notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
